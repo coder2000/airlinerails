086fb12e262eaf2cb527e4922397cf1b17a40140
Dieter Lunn <coder2000@gmail.com>
2018-12-22 00:33:38 -0600

    add coverage and test tasks

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,9 @@
 # files.
 
 require 'cucumber/rails'
+require 'coveralls'
+
+Coveralls.wear_merged!('rails')
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
