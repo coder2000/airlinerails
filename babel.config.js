f4536c61bc205cad3e21d16595b6905fd8a46228
Dieter Lunn <coder2000@gmail.com>
2018-12-22 02:04:35 -0600

    update packages with babel plugins

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,18 @@
-module.exports = function (api) {
-  let validEnv = ['development', 'test', 'production'];
-  let currentEnv = api.env();
-  let isDevelopmentEnv = api.env('development');
-  let isProductionEnv = api.env('production');
-  let isTestEnv = api.env('test');
+/* eslint global-require: "off" */
+
+module.exports = function babelConfig(api) {
+  const validEnv = ['development', 'test', 'production'];
+  const currentEnv = api.env();
+  const isDevelopmentEnv = api.env('development');
+  const isProductionEnv = api.env('production');
+  const isTestEnv = api.env('test');
 
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
-      `${'Please specify a valid `NODE_ENV` or ' +
-      '`BABEL_ENV` environment variables. Valid values are "development", ' +
-      '"test", and "production". Instead, received: '}${ 
-      JSON.stringify(env) 
+      `${'Please specify a valid `NODE_ENV` or '
+      + '`BABEL_ENV` environment variables. Valid values are "development", '
+      + '"test", and "production". Instead, received: '}${
+        JSON.stringify(currentEnv)
       }.`,
     );
   }
