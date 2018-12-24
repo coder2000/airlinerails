66f8376cbd6077f76b33dad1b5ec11df3d5a38cd
Dieter Lunn <coder2000@gmail.com>
2018-12-24 16:07:18 -0600

    design home page

--- app/javascript/main/modules/app/Home.jsx
+++ app/javascript/main/modules/app/Home.jsx
@@ -1,7 +1,29 @@
 // @flow
+/* eslint react/jsx-one-expression-per-line: off */
 
 import * as React from 'react';
+import {
+  Hero, HeroHeader, HeroBody, HeroFooter, Container, Title, Button,
+} from 'bloomer';
 
 export default function Home() {
-  return <div>Hello from Home component</div>;
+  return (
+    <Hero isSize="medium" isColor="dark" isFullHeight>
+      <HeroHeader>
+        <div>The Airline</div>
+      </HeroHeader>
+      <HeroBody>
+        <Container hasTextAlign="centered">
+          <Title>The Airline</Title>
+        </Container>
+      </HeroBody>
+      <HeroFooter style={{ marginBottom: '10vh' }}>
+        <Container hasTextAlign="centered">
+          <Button isColor="info" isSize="large">
+            Get Started
+          </Button>
+        </Container>
+      </HeroFooter>
+    </Hero>
+  );
 }
