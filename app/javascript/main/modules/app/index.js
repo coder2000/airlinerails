bbbcd787b11013cc8ad58673444c8af8e3d98251
Dieter Lunn <coder2000@gmail.com>
2018-12-24 21:14:25 -0600

    reorganized application into frontend and api

--- app/javascript/main/modules/app/index.js
+++
@@ -1,4 +0,0 @@
-// @flow
-
-export { default as App } from './App';
-export { default as Home } from './Home';
--- spec/javascript/setupTests.js
+++
@@ -1,6 +0,0 @@
-// @flow
-
-import Enzyme from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
