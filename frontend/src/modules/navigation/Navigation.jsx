c61d86b6923fa808e3a720095ecb238043b517d8
Dieter Lunn <coder2000@gmail.com>
2018-12-25 02:48:38 -0600

    update line endings

--- frontend/src/modules/navigation/Navigation.jsx
+++ frontend/src/modules/navigation/Navigation.jsx
@@ -1,31 +1,26 @@
 // @flow
 /* eslint react/jsx-one-expression-per-line: off */
 
-import * as React from "react";
+import * as React from 'react';
 import {
-  Navbar,
-  NavbarBrand,
-  NavbarBurger,
-  NavbarMenu,
-  NavbarItem,
-  NavbarEnd
-} from "bloomer";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  Navbar, NavbarBrand, NavbarBurger, NavbarMenu, NavbarItem, NavbarEnd,
+} from 'bloomer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 type Props = {};
 
 type State = {
-  isActive: boolean
+  isActive: boolean,
 };
 
 export default class Navigation extends React.Component<Props, State> {
   state = {
-    isActive: false
+    isActive: false,
   };
 
   setActive() {
     this.setState(previous => ({
-      isActive: !previous.isActive
+      isActive: !previous.isActive,
     }));
   }
 
