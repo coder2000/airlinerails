7644b12662a66c68cce39fb4bb2bed5531433dd5
Dieter Lunn <coder2000@gmail.com>
2018-12-27 15:13:51 -0600

    start new game form

--- frontend/src/modules/game/NewGame.jsx
+++ frontend/src/modules/game/NewGame.jsx
@@ -1,6 +1,9 @@
 // @flow
 
 import * as React from 'react';
+import {
+  Container, Column, Columns, Box, Field, Label, Control, Input, Title,
+} from 'bloomer';
 import GameContext from '../contexts';
 
 type Props = {};
@@ -15,6 +18,37 @@ export default class NewGame extends React.Component<Props> {
   }
 
   render() {
-    return <div>New Game</div>;
+    return (
+      <Container style={{ marginTop: '10vh' }}>
+        <Columns isCentered>
+          <Column isSize="2/3">
+            <Box>
+              Welcome, stranger. So you want to start an airline, well you have come to the right
+              place. Tell us a bit about your new venture.
+            </Box>
+          </Column>
+        </Columns>
+        <Columns isCentered>
+          <Column isSize="1/3">
+            <Title isSize={4}>Airline</Title>
+            <Field>
+              <Label>Name</Label>
+              <Control>
+                <Input type="text" />
+              </Control>
+            </Field>
+          </Column>
+          <Column isSize="1/3">
+            <Title isSize={4}>Owner</Title>
+            <Field>
+              <Label>Name</Label>
+              <Control>
+                <Input type="text" />
+              </Control>
+            </Field>
+          </Column>
+        </Columns>
+      </Container>
+    );
   }
 }
