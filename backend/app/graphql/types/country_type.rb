063831695a42d751abd42cb3e056d3627c0430d9
Dieter Lunn <coder2000@gmail.com>
2018-12-27 18:48:19 -0600

    add countries and query to return all

---
+++ backend/app/graphql/types/country_type.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Types
+  class CountryType < Types::BaseObject
+    graphql_name 'Country'
+    field :name, String, null: false
+    field :code, String, null: false
+
+    def code
+      object.alpha2
+    end
+  end
+end
--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -1,13 +1,18 @@
+# frozen_string_literal: true
+
 module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    graphql_name 'Query'
+
+    field :countries, [Types::CountryType], null: true do
+      description 'Get a list of countries'
+    end
+
+    def countries
+      ISO3166::Country.all
     end
   end
 end
