221417a088b43a401d5a67d0f3d47f71c0de2d15
Dieter Lunn <coder2000@gmail.com>
2018-12-31 04:20:30 -0600

    add spec for loading component

---
+++ frontend-next/src/modules/app/__tests__/Loading.spec.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import Loading from '../Loading';
+
+test('Loading component renders without error', () => {
+  const loading = shallow(<Loading />);
+
+  expect(loading).toBeDefined();
+});
