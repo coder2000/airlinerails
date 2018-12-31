36d3bb2d4d7e475c2cd976f0169e86140569f09e
Dieter Lunn <coder2000@gmail.com>
2018-12-31 02:50:54 -0600

    add webpack plugins

--- frontend-next/webpack.prod.js
+++ frontend-next/webpack.prod.js
@@ -1,6 +1,18 @@
-const plugins = [];
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+const plugins = [new CompressionPlugin()];
 
 module.exports = require('./webpack.config')({
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        uglifyOptions: {
+          extractComments: 'all',
+        },
+      }),
+    ],
+  },
   plugins,
 });
