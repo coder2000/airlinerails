62446355697d9924becb34d5f950813680642cc2
Dieter Lunn <coder2000@gmail.com>
2018-12-31 04:15:41 -0600

    configure jest for coverage

--- frontend-next/jest.config.js
+++ frontend-next/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   setupTestFrameworkScriptFile: '<rootDir>/src/tests/setup.js',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
 };
